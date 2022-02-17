1e2846e895669d3bd09afb123eeb18f21f75ef84
Sadikasadhi <97940468+Sadikasadhi@users.noreply.github.com>
2022-02-17 10:46:15 +0530

    Add files via upload

---
+++ AmicableNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System; 
+
+namespace AmicableNumber
+{
+    class AmicableNumber
+    {
+        static void Main(String[] args)
+        {
+            int num1, num2, sum1 = 0, sum2 = 0;
+            Console.WriteLine("\n -----AMICABLE NUMBERS-------\n");
+            Console.Write("Enter the  First Number : ");
+            num1 =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the  Second Number : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<num1;i++)
+            {
+                if (num1%i == 0)
+                {
+                    sum1 += i;
+                }
+            }
+            for (int i=1;i<num2;i++)
+            {
+                if (num2 % i == 0)
+                {
+                    sum2 += i;
+                }
+            }
+            if (sum1 == num2 && sum2 == num1)
+            {
+                Console.WriteLine("The numbers{0} and {1} are amicable", num1,num2);
+            }
+            else
+            {
+                Console.WriteLine("The numbers{0} and {1} are not amicable", num1, num2);
+            }
+        }
+    }
+}
