b80bce0ba3be8d5501e5c3abe3db8e4d5da210c9
Sadikasadhi <97940468+Sadikasadhi@users.noreply.github.com>
2022-02-25 12:09:34 +0530

    Add files via upload

---
+++ Palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.Write("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while(n>0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.WriteLine("Number is Palindrome");
+            else
+                Console.WriteLine("Number is not Palindrome");
+        }
+    }
+}
